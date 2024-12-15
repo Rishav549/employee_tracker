cdc12af964023b67936a5a90990e42eb8c65d3bb
Rishav <rishavthakkar@gmail.com>
2024-12-15 20:29:59 +0530

    feat: error mssg properly printed

--- lib/repo/attendance.dart
+++ lib/repo/attendance.dart
@@ -9,11 +9,7 @@ final Dio _dio = GetIt.I<Dio>();
 Future<void> upload(AttendanceModel data) async {
   try {
     await _dio.post("${UrlConfig.baseurl}/attendance/", data: data.toJson());
-  } on DioException catch (e) {
-    if (e.response?.statusCode == 422) {
-      CustomLogger.error("Unprocessable Entity: ${e.response?.data}");
-    } else {
-      CustomLogger.error("Unexpected error: ${e.message}");
-    }
+  } catch (e) {
+    CustomLogger.error(e);
   }
 }
